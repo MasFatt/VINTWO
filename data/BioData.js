bd1d7cf2b5bee328fd5d7450376f15f6ed3be535
Fatiha <fatihaerosperdana@gmail.com>
2025-06-25 20:03:22 +0700

    First commit

---
+++ data/BioData.js
@@ -0,0 +1,25 @@
+// date: 1 Aug, 2022
+const bioData = [
+    {
+        name: 'VINTWO | CLOTHING BRAND',
+        username: 'VINTWO | CLOTHING BRAND',
+        url: '#',
+        titleImg: true,
+        avatar: '/avatar.png',
+        nftAvatar: true,
+        description: 'Dibuat untuk mereka yang berani bersuara, melawan arus, dan menolak diam. Setiap detail mencerminkan semangat pemberontakan dan kebebasan berekspresi. ',
+        descShow: true,
+        // subdesc: 'Use this template and deploy on vercel for free',
+        subdescShow: true,
+        newProductUrl: 'https://3dicons.co',
+        newProduct: true,
+        author: 'fatiha',
+        authorURL: 'https://dribbble.com/fatihaers',
+        footerText: 'made by'
+    },
+];
+
+export default bioData;
+
+
+
---
+++ next-seo.config.js
@@ -0,0 +1,23 @@
+export default {
+    openGraph: {
+        type: 'website',
+        locale: 'en_EN',
+        title: 'next lnks by realvjy',
+        url: 'https://vjy.me',
+        description: 'Dibuat untuk mereka yang berani bersuara, melawan arus, dan menolak diam. Setiap detail mencerminkan semangat pemberontakan dan kebebasan berekspresi.',
+        keywords: 'designer, product designer, illustrator, indian designer, vijay verma, realvjy, illlustrations, indian illustrator, ui designer india, sushi design system, zomato designer, 3dicons, uiprint, vijay realvjy, indian ui designer, design india, overlayz, nft art',
+        images: [
+            {
+                width: 1200,
+                height: 630,
+                url: `https://vjy.me/preview.jpg`,
+            },
+        ],
+        site_name: 'realvjy',
+    },
+    twitter: {
+        handle: '@realvjy',
+        site: 'vjy.me',
+        cardType: 'summary_large_image',
+    },
+};
