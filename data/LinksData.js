a448e4b758d54f7cf6dc420f999d309c60e100d9
Fatiha <fatihaerosperdana@gmail.com>
2025-06-25 20:18:42 +0700

    Pesan deskriptif tentang perubahan

--- data/LinksData.js
+++ data/LinksData.js
@@ -3,138 +3,138 @@ const webLinks = [
     // All social profile
     {
         title: 'Twitter',
-        url: 'https://twitter.com/realvjy',
+        url: '#',
         type: 'social',
         icon: '/twitter.svg',
         on: true
     },
     {
         title: 'Instagram',
-        url: 'https://instagram.com/realvjy',
+        url: '#',
         type: 'social',
         icon: '/insta.svg',
         on: true
     },
     {
         title: 'Dribbble',
-        url: 'https://dribbble.com/realvjy',
+        url: '#',
         type: 'social',
         icon: '/dribbble.svg',
         on: false
     },
     {
         title: 'Medium',
-        url: 'https://medium.com/@realvjy',
+        url: '#',
         type: 'social',
         icon: '/medium.svg',
         on: false
     },
     {
         title: 'Github',
-        url: 'https://github.com/realvjy',
+        url: '#',
         type: 'social',
         icon: '/github.svg',
         on: true
     },
     {
         title: 'Youtube',
-        url: 'https://youtube.com/realvjy',
+        url: '#',
         type: 'social',
         icon: '/youtube.svg',
         on: false
     },
     {
         title: 'Figma',
-        url: 'https://figma.com/@realvjy',
+        url: '#',
         type: 'social',
         icon: '/figma.svg',
         on: true
     },
     {
         title: 'Behance',
-        url: 'https://behance.net/realvjy',
+        url: '#',
         type: 'social',
         icon: '/behance.svg',
         on: false
     },
     {
         title: 'Linkedin',
-        url: 'https://www.linkedin.com/in/realvjy/',
+        url: '#',
         type: 'social',
         icon: '/linkedin.svg',
         on: false
     },
     {
         title: 'Mastodon',
-        url: 'https://mastodon.online/@realvjy',
+        url: '#',
         type: 'social',
         icon: '/mastodon.svg',
         on: true
     },
     {
         title: 'Hive Social',
-        url: 'https://www.hivesocial.app/?@realvjy',
+        url: '#',
         type: 'social',
         icon: '/hive.svg',
         on: false
     },
     {
         title: 'Post',
-        url: 'https://www.post.news/realvjy',
+        url: '#',
         type: 'social',
         icon: '/post.svg',
         on: false
     },
     {
         title: 'foundation',
-        url: 'https://vjy.me',
+        url: '#',
         type: 'nft',
         icon: '/foundation.svg',
         on: false
     },
     {
         title: 'opensea',
-        url: 'https://vjy.me',
+        url: '#',
         type: 'nft',
         icon: '/opensea.svg',
         on: false
     },
     {
         title: 'new collection',
-        url: 'https://vjy.me',
+        url: '#',
         type: 'nft',
         icon: '/opensea.svg',
         on: false
     },
     // Install section
     {
-        title: 'Use this template',
-        url: 'https://github.com/realvjy/nxt-lnk/generate',
+        title: 'SHOPEE VINTWO',
+        url: 'http://shopee.co.id/vintwo',
         type: 'install',
         icon: '/use.png',
         on: true
     },
     {
-        title: 'Deploy on Vercel',
-        url: 'https://vercel.com/import/git?s=https://github.com/realvjy/nxt-lnk',
+        title: 'TIKTOK VINTWO',
+        url: 'https://www.tiktok.com/@vintwo.official',
         type: 'install',
         icon: '/vercel.svg',
         on: true
     },
     {
-        title: 'Documentation',
-        url: 'https://github.com/realvjy/nxt-lnk#nxt-lnk',
+        title: 'WHATSAPP (ADMIN)',
+        url: 'https://wa.me/625124244985',
         type: 'install',
         icon: '/doc.png',
         on: true
     },
-    {
-        title: 'Github',
-        url: 'https://github.com/realvjy/nxt-lnk',
-        type: 'install',
-        icon: '/github-fill.svg',
-        on: true
-    },
+    // {
+    //     title: 'Github',
+    //     url: 'https://github.com/realvjy/nxt-lnk',
+    //     type: 'install',
+    //     icon: '/github-fill.svg',
+    //     on: true
+    // },
 
 //     {
 //         title: 'Buy Me a Coffee',
